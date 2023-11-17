82d2faf4534dc310bd543aebc9c76dd5f2ce123e
alv18051 <alv18051@uvg.edu.gt>
2023-11-16 19:19:04 -0600

    si3

--- ds.py
+++ ds.py
@@ -10,10 +10,8 @@ from sklearn.pipeline import Pipeline
 from tensorflow.keras.layers import Input, Dense, Concatenate
 from tensorflow.keras.models import Model
 
-# Cargar datos
 df = pd.read_csv('datos_bundesliga.csv')
 
-# Separar características y etiquetas
 X = df.drop('Pases', axis=1)
 y = df['Pases']
 
@@ -24,6 +22,16 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 numeric_features = ['Minuto', 'Segundo']
 X_train_numeric = X_train[numeric_features]
 X_test_numeric = X_test[numeric_features]
+
+# Escalar características numéricas
+scaler = StandardScaler()
+X_train_numeric = scaler.fit_transform(X_train_numeric)
+X_test_numeric = scaler.transform(X_test_numeric)
+
+# Características categóricas
+categorical_features = ['Equipo', 'Posicion_Inicio', 'Fase_del_Partido']
+X_train_categorical = X_train[categorical_features]
+X_test_categorical = X_test[categorical_features]
 
 
 
